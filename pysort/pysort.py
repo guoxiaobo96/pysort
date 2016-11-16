328ce8cc335cb48ffb3477bcaba2dac2f208e83a
guoxiaobo96 <guoxiaobo96@gmail.com>
2016-11-16 11:01:12 +0800

    归并排序

--- pysort/pysort.py
+++ pysort/pysort.py
@@ -1,4 +1,4 @@
-sample=[5,2,4,6,1,3]
+sample=[5,2,4,1,3,6]
 def insert_sort(smaple):
     result=[smaple[0]]
     j=1
@@ -14,6 +14,32 @@ def insert_sort(smaple):
     return(result)
 
 def merge_sort(sample):
-    pass
-print(insert_sort(sample))
+    length=len(sample)
+    middle=length/2
+    i=0
+    j=int(middle)
+    k=0
+    left=[]
+    right=[]
+    result=[]
+    while i < middle:
+        left.append(sample[i])
+        i=i+1
+    left.append(10000)
+    while j<length :
+        right.append(sample[j])
+        j=j+1
+    right.append(10000)
+    i=0
+    j=0
+    while k<length:
+        if left[i]>right[j]:
+            result.append(right[j])
+            j=j+1
+        else:
+            result.append(left[i])
+            i=i+1
+        k=k+1
+    return (result)
+print(merge_sort(sample))
 
