1b6c592859ea6b65bad8a47da4da1d97c6e4597b
guoxiaobo96 <guoxiaobo96@gmail.com>
2016-11-17 16:21:51 +0800

    堆排序

--- pysort/pysort.py
+++ pysort/pysort.py
@@ -1,4 +1,4 @@
-sample=[2,7,5,6,2,4,10,1,3]
+sample=[4,1,3,2,16,9,31,10,14,8,7]
 def insert_sort(smaple):
     result=[smaple[0]]
     j=1
@@ -40,5 +40,57 @@ def merge_sort(sample):
 
     return(merge_process(sample))
 
-print(merge_sort(sample))
+def heap_sort(heap):
+    def max_heapify(heap,i):
+        heap_size=len(heap)
+        left=2*i+1
+        right=2*i+2
+        if left<heap_size and heap[left]>heap[i]:
+            largest=heap[left]
+            num=left
+        else:
+            largest=heap[i]
+
+        if right<heap_size and heap[right]>largest:
+            largest=heap[right]
+            num=right
+        else:
+            largest=largest
+
+        if largest!=heap[i]:
+            heap[num]=heap[i]
+            heap[i]=largest
+            max_heapify(heap,num)
+
+
+    def build_max_heap(heap):
+        heap_size=len(heap)
+        i=int(heap_size/2)-1
+        while i>=0:
+            max_heapify(heap,i)
+            i-=1
+        return(heap)
+
+    def sort(heap):
+        max_heap=build_max_heap(heap)
+        i=len(max_heap)-1
+        j=0
+        temp=[]
+        while j<=i:
+            temp.append(max_heap[j])
+            j+=1
+        while i>0:
+            max_heap[i]=temp[0]
+            temp[0]=temp[i]
+            temp=temp[:i]
+            max_heapify(temp,0)
+            i=i-1
+        max_heap[0]=temp[0]
+        return (max_heap)
+        
+    return sort(heap)
+
+
+
+print(heap_sort(sample))
 
